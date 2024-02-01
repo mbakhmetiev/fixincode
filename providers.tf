19129473a8ceb7cd277179de8233fc46b76289e2
mbakhmetiev <mbakhmetiev@yandex.ru>
2024-02-01 21:41:37 +0100

    first commit

---
+++ providers.tf
@@ -0,0 +1,14 @@
+# Terraform Providers
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
